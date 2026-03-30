89e158bfb2c4505aa2a25d139bcba6c0e5e61585
S-A-Adit <syedafnanadit@gmail.com>
2026-03-29 21:12:01 -0400

    RESET MADE

---
+++ server/config/reset.js
@@ -0,0 +1,22 @@
+import { pool } from './database.js'
+
+const createTable = async () => {
+    await pool.query(`
+        DROP TABLE IF EXISTS cars;
+
+        CREATE TABLE cars (
+            id       SERIAL PRIMARY KEY,
+            make     VARCHAR(50)    NOT NULL,
+            model    VARCHAR(50)    NOT NULL,
+            color    VARCHAR(50)    NOT NULL,
+            wheels   VARCHAR(50)    NOT NULL,
+            engine   VARCHAR(50)    NOT NULL,
+            interior VARCHAR(50)    NOT NULL,
+            price    NUMERIC(10, 2) NOT NULL
+        );
+    `)
+    console.log('cars table created')
+    await pool.end()
+}
+
+createTable()
