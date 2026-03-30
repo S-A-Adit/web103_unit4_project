304be2412d7d72d327cd7cb0dda4acf57181d26b
S-A-Adit <syedafnanadit@gmail.com>
2026-03-29 21:46:19 -0400

    Problems fixed

--- server/config/reset.js
+++ server/config/reset.js
@@ -1,6 +1,11 @@
-import { pool } from './database.js'
+import dotenv from 'dotenv'
+import { fileURLToPath } from 'url'
+dotenv.config({ path: fileURLToPath(new URL('../.env', import.meta.url)) })
+
+const { pool } = await import('./database.js')
 
 const createTable = async () => {
+
     await pool.query(`
         DROP TABLE IF EXISTS cars;
 
